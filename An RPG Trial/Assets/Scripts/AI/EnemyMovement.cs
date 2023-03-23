3907b98b11efd7bf2af24de24461e42073c75d20
YelizKB <yelizkurt23@gmail.com>
2023-03-23 23:32:10 +0000

    AI FOV Player Detection

---
+++ An RPG Trial/Assets/Scripts/AI/EnemyMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public List<Transform> enemyPatrolPoints;
+
+    public bool isPlayerDetected;
+
+    public float FOVRadius;
+    [Range(0,360)]
+    public float FOVAngle;
+
+    public GameObject playerRef;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    private void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, FOVRadius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < FOVAngle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+                    isPlayerDetected = true;
+                }
+                else isPlayerDetected = false;
+            }
+            else { isPlayerDetected = false; }
+        }
+        else if (isPlayerDetected) isPlayerDetected = false;
+    }
+}
